4fd9d49f6c09bfc42ba68948b914fc3d048db799
mlvrkhn <martin.guide@gmail.com>
2020-02-22 19:31:44 +0100

    first

---
+++ learning_templates/basic_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context_dict = {'txt':'hello world', 'number':998}
+    return render(request, 'basic_app/index.html', context_dict)
+
+def other(request):
+    return render(request, 'basic_app/other.html')
+
+def relative(request):
+    return render(request, 'basic_app/relative_url_templates.html')
